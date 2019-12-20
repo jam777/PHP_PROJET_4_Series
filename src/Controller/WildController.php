285edcee5cefd4220a2316d3512f5debb2604204
lab <jojeux77@tutanota.com>
2019-12-20 17:24:08 +0100

    Création de la méthode show() qui affiche la page 'Détaille Série' en y passant le slug comme titre

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -18,17 +18,32 @@ class WildController extends AbstractController
     }
 
     /**
-     * @Route("/wild/show/{slug}", name="wild_show")
+     * @Route("/wild/show/{slug}", name="wild_show", requirements={"slug"="[a-z0-9-]+"},defaults={"slug"="serie-sans-titre"})
      */
     public function show($slug)
     {   
-
-        //Slugify le titre
-
-
+        $slug=$this->slugy($slug);
+        
         return $this->render('wild/show.html.twig', [
             'current_menu' => 'Show',
+            'titre'=> $slug
         ]);
+    }
+
+    
+    /**
+     * Permet de slufguifier une phrase
+     * Critere : Remplacer "-" par des " "
+     *           Premiere lettre de chaque mot par des majuscule   
+     *
+     * @param string $slug
+     * @return string
+     */    
+    private function slugy(string $slug) :string
+    {   
+        $slug=str_replace("-"," ",$slug);
+        $slug=ucwords($slug," ");
+        return $slug;
     }
 
 
