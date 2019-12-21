1be1ca13c984d16f4e61673e402ff3942f320ff5
lab <jojeux77@tutanota.com>
2019-12-21 16:32:12 +0100

    Création de la vue Categorie qui affiche les series par catégorie

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CategoryRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
-{
+{   
+    protected $repoCategory;
+
+    public function __construct(CategoryRepository $repoCategory)
+    {
+        $this->repoCategory = $repoCategory;
+    }
+
+
+    
+
     /**
      * @Route("/", name="app_index")
      */
     public function index()
-    {
+    {   
+        $categories=$this->repoCategory->findAll();
+
         return $this->render('default/home.html.twig', [
             'current_menu' => '',
+            'categories'   => $categories 
         ]);
     }
 }
--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -2,40 +2,91 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WildController extends AbstractController
 {   
-        
+    
+    private $repoProgram,$repoCategory,$categories;
+    
+    public function __construct(ProgramRepository $repoProgram,CategoryRepository $repoCategory)
+    {
+        $this->repoProgram=$repoProgram;
+        $this->repoCategory=$repoCategory;
+        $this->categories=$this->repoCategory->findAll();
+    }   
+
+    /*Affiche toutes les Séries*/
+
     /**
      * @Route("/wild", name="wild_index")
      */
-    public function index(ProgramRepository $repo)
+    public function index()
     {
-        $programs=$repo->findAll();
+        $programs=$this->repoProgram->findAll();
+
 
         return $this->render('wild/index.html.twig', [
             'current_menu' => 'Accueil',
+            'categories'   => $this->categories,
             'programs'     => $programs 
         ]);
     }
 
+     /*Affiche le détail d'une Série*/
+
     /**
      * @Route("/wild/show/{slug}", name="wild_show", requirements={"slug"="[a-z0-9-]+"},defaults={"slug"="serie-sans-titre"})
      */
-    public function show($slug,ProgramRepository $repo)
+    public function show($slug)
     {   
         $slug=$this->slugy($slug);
 
-        $program=$repo->findOneByTitle($slug);
+        $program=$this->repoProgram->findOneByTitle($slug);
               
         return $this->render('wild/show.html.twig', [
             'current_menu' => 'Show',
+            'categories'   => $this->categories,
             'program'=> $program
         ]);
     }
+
+     /*Affiche toutes les Séries d'une Categorie*/
+
+    /**
+     * @Route("/category/{categoryName}",name="show_category")
+     */
+    public function showByCategory($categoryName){
+        
+        $category=$this->repoCategory->findOneByName($categoryName);
+      
+
+        if(!$category)
+        {
+            throw $this->createNotFoundException(
+                "Cette categorie n'existe pas."
+            );
+        }        
+
+        $programs=$this->repoProgram->findBy(
+            ['category'=>$category],
+            ['id'=>'DESC'],
+            3            
+        );
+
+        return $this->render("wild/category.html.twig",[
+            'current_menu' => 'Category',
+            'categories'   => $this->categories,
+            'category'    => $category,
+            'programs'      => $programs  
+            
+        ]);
+
+    }
+
 
     
     /**
